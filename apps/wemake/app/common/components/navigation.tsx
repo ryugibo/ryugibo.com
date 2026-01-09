485fc7915224ef1af9938718e141f0554bd3c6f8
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-01-09 13:59:42 +0900

    feat(wemake): navigation menus

---
+++ apps/wemake/app/common/components/navigation.tsx
@@ -0,0 +1,183 @@
+import { Link } from "react-router";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "./ui/navigation-menu";
+import { Separator } from "./ui/separator";
+import { cn } from "~/lib/utils";
+
+const menus = [
+  {
+    name: "Products",
+    to: "/products",
+    items: [
+      {
+        name: "Leaderboards",
+        description: "See the top performers in your community",
+        to: "/products/leaderboards",
+      },
+      {
+        name: "Categories",
+        description: "See the top categories in your community",
+        to: "/products/categories",
+      },
+      {
+        name: "Search",
+        description: "Search for a product",
+        to: "/products/search",
+      },
+      {
+        name: "Submit a Product",
+        description: "Submit a product to our community",
+        to: "/products/submit",
+      },
+      {
+        name: "Promote",
+        description: "Promote a product to our community",
+        to: "/products/promote",
+      },
+    ],
+  },
+  {
+    name: "Jobs",
+    to: "/jobs",
+    items: [
+      {
+        name: "Remote Jobs",
+        description: "Find a remote job in our community",
+        to: "/jobs?location=remote",
+      },
+      {
+        name: "Full-Time Jobs",
+        description: "Find a full-time job in our community",
+        to: "/jobs?type=full-time",
+      },
+      {
+        name: "Freelance Jobs",
+        description: "Find a freelance job in our community",
+        to: "/jobs?type=freelance",
+      },
+      {
+        name: "Internships",
+        description: "Find an internship in our community",
+        to: "/jobs?type=internship",
+      },
+      {
+        name: "Submit a Job",
+        description: "Submit a job to our community",
+        to: "/jobs/submit",
+      },
+    ],
+  },
+  {
+    name: "Community",
+    to: "/community",
+    items: [
+      {
+        name: "All Posts",
+        description: "See all posts in our community",
+        to: "/community",
+      },
+      {
+        name: "Top Posts",
+        description: "See the top posts in our community",
+        to: "/community?sort=top",
+      },
+      {
+        name: "New Posts",
+        description: "See the new posts in our community",
+        to: "/community?sort=new",
+      },
+      {
+        name: "Create a Post",
+        description: "Create a post in our community",
+        to: "/community/create",
+      },
+    ],
+  },
+  {
+    name: "IdeasGPT",
+    to: "/ideas",
+  },
+  {
+    name: "Teams",
+    to: "/teams",
+    items: [
+      {
+        name: "All Teams",
+        description: "See all teams in our community",
+        to: "/teams",
+      },
+      {
+        name: "Create a Team",
+        description: "Create a team in our community",
+        to: "/teams/create",
+      },
+    ],
+  },
+];
+
+export default function Navigation() {
+  return (
+    <nav className="flex px-20 h-16 items-center justify-between backdrop-blur fixed top-0 left-0 right-0 z-50 bg-background/50">
+      <div className="flex items-center">
+        <Link to="/" className="font-bold tracking-tight text-lg">
+          wemake
+        </Link>
+        <div className="h-6 mx-4">
+          <Separator orientation="vertical" />
+        </div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            {menus.map((menu) => (
+              <NavigationMenuItem key={menu.name}>
+                {menu.items ? (
+                  <>
+                    <Link to={menu.to}>
+                      <NavigationMenuTrigger>{menu.name}</NavigationMenuTrigger>
+                    </Link>
+                    <NavigationMenuContent>
+                      <ul className="grid w-[600px] font-light gap-3 p-4 grid-cols-2">
+                        {menu.items?.map((item) => (
+                          <NavigationMenuItem
+                            key={item.name}
+                            className={cn([
+                              "select-none rounded-md transition-colors focus:bg-accent hover:bg-accent",
+                              (item.to === "/products/promote" || item.to === "/jobs/submit") &&
+                                "col-span-2 bg-primary/10 focus:bg-primary/20 hover:bg-primary/20",
+                            ])}
+                          >
+                            <NavigationMenuLink>
+                              <Link
+                                to={item.to}
+                                className="p3 space-y-1 block leading-none no-underline outline-none"
+                              >
+                                <span className="text-sm font-medium leading-none">
+                                  {item.name}
+                                </span>
+                                <p className="text-sm text-muted-foreground">{item.description}</p>
+                              </Link>
+                            </NavigationMenuLink>
+                          </NavigationMenuItem>
+                        ))}
+                      </ul>
+                    </NavigationMenuContent>
+                  </>
+                ) : (
+                  <Link to={menu.to} className={navigationMenuTriggerStyle()}>
+                    {menu.name}
+                  </Link>
+                )}
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+      </div>
+    </nav>
+  );
+}
