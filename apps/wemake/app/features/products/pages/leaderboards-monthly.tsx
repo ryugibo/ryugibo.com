3cad0583dae5646115a022e2eee1945f239860ab
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-01-11 23:58:36 +0900

    feat(wemake): Monthly leaderboards

--- apps/wemake/app/features/products/pages/leaderboards-monthly.tsx
+++ apps/wemake/app/features/products/pages/leaderboards-monthly.tsx
@@ -1,14 +1,95 @@
+import { DateTime } from "luxon";
+import { data, isRouteErrorResponse, Link } from "react-router";
+import { z } from "zod";
+import { Hero } from "~/common/components/hero";
+import { ProductPagination } from "~/common/components/product-pagination";
+import { Button } from "~/common/components/ui/button";
+import { ProductCard } from "~/features/products/components/product-card";
 import type { Route } from "./+types/leaderboards-monthly";
 
-export const meta: Route.MetaFunction = () => [
-  { title: "Monthly Leaderboard | wemake" },
-  { name: "description", content: "Monthly Product Leaderboard" },
-];
+export function meta(_: Route.MetaArgs) {
+  return [
+    { title: "Daily Leaderboard | wemake" },
+    { name: "description", content: "Daily Product Leaderboard" },
+  ];
+}
 
-export default function LeaderboardsMonthlyPage() {
+const paramsSchema = z.object({
+  year: z.coerce.number(),
+  month: z.coerce.number(),
+});
+
+export function loader({ params }: Route.LoaderArgs) {
+  const { success, data: parsedData } = paramsSchema.safeParse(params);
+  if (!success) {
+    throw data({ error_code: "invalid_date", message: "invalid date" }, { status: 400 });
+  }
+  const date = DateTime.fromObject(parsedData);
+  if (!date.isValid) {
+    throw data({ error_code: "invalid_date", message: "invalid date" }, { status: 400 });
+  }
+  const today = DateTime.now().startOf("month");
+  if (date > today) {
+    throw data({ error_code: "future_date", message: "future date" }, { status: 400 });
+  }
+  return {
+    ...parsedData,
+  };
+}
+
+const products = Array.from({ length: 11 }).map((_, index) => ({
+  id: `productId-${index}`,
+  title: "Product Title",
+  description: "Product Description",
+  commentsCount: 12,
+  viewsCount: 12,
+  upvotesCount: 120,
+}));
+
+export default function LeaderboardsMonthlyPage({ loaderData }: Route.ComponentProps) {
+  const urlDate = DateTime.fromObject(loaderData);
+  const prevDate = urlDate.minus({ month: 1 });
+  const nextDate = urlDate.plus({ month: 1 });
+  const isToday = urlDate.equals(DateTime.now().startOf("month"));
   return (
     <div className="space-y-10">
-      <h1 className="text-4xl font-bold">Monthly Leaderboard</h1>
+      <Hero
+        title={`Best of month ${urlDate.startOf("month").toLocaleString({ month: "long", year: "numeric" })}`}
+      />
+      <div className="flex items-center justify-center gap-2">
+        <Button variant="secondary">
+          <Link to={`/products/leaderboards/monthly/${prevDate.year}/${prevDate.month}`}>
+            &larr; {prevDate.toLocaleString({ month: "long", year: "numeric" })}
+          </Link>
+        </Button>
+        {!isToday && (
+          <Button variant="secondary">
+            <Link to={`/products/leaderboards/monthly/${nextDate.year}/${nextDate.month}`}>
+              {nextDate.toLocaleString({ month: "long", year: "numeric" })} &rarr;
+            </Link>
+          </Button>
+        )}
+      </div>
+      <div className="space-y-5 w-full max-w-3xl mx-auto">
+        {products.map((product) => (
+          <ProductCard key={product.id} {...product} />
+        ))}
+      </div>
+      <ProductPagination totalPages={10} />
     </div>
   );
+}
+
+export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
+  if (isRouteErrorResponse(error)) {
+    return (
+      <div>
+        {error.data.message} / {error.data.error_code}
+      </div>
+    );
+  }
+  if (error instanceof Error) {
+    return <div>{error.message}</div>;
+  }
+  return <div>Unknown error</div>;
 }
