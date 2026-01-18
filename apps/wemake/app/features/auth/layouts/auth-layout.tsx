0e4771d119a00e1a1c4c66609a5a2e201403402c
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-01-18 23:25:57 +0900

    feat(wemake): Add flikering grid into Auth layout

--- apps/wemake/app/features/auth/layouts/auth-layout.tsx
+++ apps/wemake/app/features/auth/layouts/auth-layout.tsx
@@ -1,9 +1,17 @@
 import { Outlet } from "react-router";
+import { FlickeringGrid } from "~/common/components/ui/flickering-grid";
 
 export default function AuthLayout() {
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 h-screen">
-      <div className="hidden lg:block bg-linear-to-br from-primary via-black to-primary/50" />
+      <FlickeringGrid
+        squareSize={4}
+        gridGap={5}
+        maxOpacity={0.5}
+        flickerChance={0.2}
+        color="#E11D49"
+        className="hidden lg:block"
+      />
       <Outlet />
     </div>
   );
