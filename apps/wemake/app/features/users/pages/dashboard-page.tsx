e1ba338b4e307fdb52e7a0f816ef0682a2e90da3
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-01-18 22:08:19 +0900

    feat(wemake): Dashboard Analytics

--- apps/wemake/app/features/users/pages/dashboard-page.tsx
+++ apps/wemake/app/features/users/pages/dashboard-page.tsx
@@ -1,14 +1,72 @@
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
+import { Card, CardContent, CardHeader, CardTitle } from "~/common/components/ui/card";
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "~/common/components/ui/chart";
 import type { Route } from "./+types/dashboard-page";
 
-export const meta: Route.MetaFunction = () => [
-  { title: "Dashboard | Wemake" },
+export const meta = (_: Route.MetaArgs) => [
+  { title: "Dashboard | wemake" },
   { name: "description", content: "Dashboard" },
 ];
 
+const chartData = [
+  { month: "January", views: 186 },
+  { month: "February", views: 305 },
+  { month: "March", views: 237 },
+  { month: "April", views: 73 },
+  { month: "May", views: 209 },
+  { month: "June", views: 214 },
+];
+
+const chartConfig = {
+  views: {
+    label: "👁️",
+    color: "var(--chart-1)",
+  },
+} satisfies ChartConfig;
+
 export default function DashboardPage() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Dashboard</h1>
+    <div className="space-y-5">
+      <h1 className="text-2xl font-semibold mb-6">Dashboard</h1>
+      <Card className="w-1/2">
+        <CardHeader>
+          <CardTitle>Profile views</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <ChartContainer config={chartConfig}>
+            <LineChart
+              accessibilityLayer
+              data={chartData}
+              margin={{
+                left: 12,
+                right: 12,
+              }}
+            >
+              <CartesianGrid vertical={false} />
+              <XAxis
+                dataKey="month"
+                tickLine={false}
+                axisLine={false}
+                tickMargin={8}
+                tickFormatter={(value) => value.slice(0, 3)}
+              />
+              <ChartTooltip cursor={false} content={<ChartTooltipContent hideLabel />} />
+              <Line
+                dataKey="views"
+                type="natural"
+                stroke="var(--color-views)"
+                strokeWidth={2}
+                dot={false}
+              />
+            </LineChart>
+          </ChartContainer>
+        </CardContent>
+      </Card>
     </div>
   );
 }
