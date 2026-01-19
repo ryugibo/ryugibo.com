88dd24547e22c108957dd9240c7a4e7dc36e66e5
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-01-20 00:31:24 +0900

    feat(wemake): Message Table

--- apps/wemake/app/features/users/schema.ts
+++ apps/wemake/app/features/users/schema.ts
@@ -1,5 +1,16 @@
-import { jsonb, pgSchema, text, timestamp, uuid } from "@ryugibo/db/core";
+import {
+  bigint,
+  boolean,
+  jsonb,
+  pgSchema,
+  primaryKey,
+  text,
+  timestamp,
+  uuid,
+} from "@ryugibo/db/core";
 import { schema } from "~/db";
+import { posts } from "~/features/community/schema";
+import { products } from "~/features/products/schema";
 
 const users = pgSchema("auth").table("users", {
   id: uuid().primaryKey(),
@@ -34,5 +45,56 @@ export const profiles = schema.table("profiles", {
 export const follows = schema.table("follows", {
   follower_id: uuid().references(() => profiles.id, { onDelete: "cascade" }),
   following_id: uuid().references(() => profiles.id, { onDelete: "cascade" }),
+  created_at: timestamp().notNull().defaultNow(),
+});
+
+export const notificationTypes = schema.enum("notification_type", [
+  "follow",
+  "review",
+  "reply",
+  "mention",
+]);
+
+export const notifications = schema.table("notifications", {
+  id: uuid().primaryKey(),
+  target_id: uuid()
+    .notNull()
+    .references(() => profiles.id, { onDelete: "cascade" }),
+  source_id: uuid().references(() => profiles.id, { onDelete: "cascade" }),
+  product_id: bigint({ mode: "number" }).references(() => products.id, { onDelete: "cascade" }),
+  post_id: bigint({ mode: "number" }).references(() => posts.id, { onDelete: "cascade" }),
+  type: notificationTypes().notNull(),
+  created_at: timestamp().notNull().defaultNow(),
+});
+
+export const messageRooms = schema.table("message_rooms", {
+  id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
+  created_at: timestamp().notNull().defaultNow(),
+});
+
+export const messageRoomMembers = schema.table(
+  "message_room_members",
+  {
+    message_room_id: bigint({ mode: "number" }).references(() => messageRooms.id, {
+      onDelete: "cascade",
+    }),
+    profile_id: uuid().references(() => profiles.id, { onDelete: "cascade" }),
+    created_at: timestamp().notNull().defaultNow(),
+  },
+  (table) => [primaryKey({ columns: [table.message_room_id, table.profile_id] })],
+);
+
+export const messages = schema.table("messages", {
+  id: bigint({ mode: "number" }).primaryKey().generatedAlwaysAsIdentity(),
+  message_room_id: bigint({ mode: "number" })
+    .notNull()
+    .references(() => messageRooms.id, {
+      onDelete: "cascade",
+    }),
+  profile_id: uuid()
+    .notNull()
+    .references(() => profiles.id, { onDelete: "cascade" }),
+  content: text().notNull(),
+  seen: boolean().notNull().default(false),
   created_at: timestamp().notNull().defaultNow(),
 });
