e37c7374366b2fb751271ae5cc69a4961bd224e6
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-01-19 22:47:28 +0900

    feat(wemake, db): create @ryugibo/db a drizzle mapper

--- apps/wemake/app/db.ts
+++ apps/wemake/app/db.ts
@@ -1,8 +1,9 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
+import { pgSchema } from "@ryugibo/db/core";
+import { drizzle } from "@ryugibo/db/driver";
+import postgres from "@ryugibo/db/postgres";
+
+export const schema = pgSchema("wemake");
 
 const client = postgres(process.env.DATABASE_URL || "", { prepare: false });
-
 const db = drizzle(client);
-
 export default db;
