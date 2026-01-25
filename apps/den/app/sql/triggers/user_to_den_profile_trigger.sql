6afa50938a3cadb39a1af936bdb92f7e0d3af412
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-01-26 01:31:36 +0900

    feat(den): create sql, user_to_den_profile_trigger

---
+++ apps/den/app/sql/triggers/user_to_den_profile_trigger.sql
@@ -0,0 +1,20 @@
+create function den.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+set search_path = ''
+as $$
+begin
+    if new.raw_app_meta_data is not null then
+        if new.raw_app_meta_data ? 'provider' AND new.raw_app_meta_data ->> 'provider' = 'email' then
+            insert into den.profiles (id, name, username, role)
+            values (new.id, 'Anonymous', 'mr.' || substr(md5(random()::text), 1, 8), 'developer');
+        end if;
+    end if;
+    return new;
+end;
+$$;
+
+create trigger user_to_den_profile_trigger
+after insert on auth.users
+for each row execute function den.handle_new_user();
