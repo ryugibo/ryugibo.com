3701bbddf9e9f398dc0b6f271dc186929619852b
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-02-03 04:39:15 +0900

    feat(den): setup RLS

--- apps/den/app/features/profile/schema.ts
+++ apps/den/app/features/profile/schema.ts
@@ -1,15 +1,44 @@
-import { authUsers } from "@ryugibo/db";
-import { text, timestamp, uuid } from "@ryugibo/db/core";
+import { authenticatedRole, authUid, authUsers, sql } from "@ryugibo/db";
+import { pgPolicy, text, timestamp, uuid } from "@ryugibo/db/core";
 import { pg } from "db";
 
-export const profiles = pg.table("profiles", {
-  id: uuid()
-    .primaryKey()
-    .references(() => authUsers.id, { onDelete: "cascade" }),
-  name: text().notNull(),
-  username: text().notNull(),
-  avatar: text(),
-  bio: text(),
-  created_at: timestamp().notNull().defaultNow(),
-  updated_at: timestamp().notNull().defaultNow(),
-});
+export const profiles = pg.table(
+  "profiles",
+  {
+    id: uuid()
+      .primaryKey()
+      .references(() => authUsers.id, { onDelete: "cascade" }),
+    name: text().notNull(),
+    username: text().notNull(),
+    avatar: text(),
+    bio: text(),
+    created_at: timestamp().notNull().defaultNow(),
+    updated_at: timestamp().notNull().defaultNow(),
+  },
+  (table) => [
+    pgPolicy("profiles-select-policy", {
+      for: "select",
+      as: "permissive",
+      to: authenticatedRole,
+      using: sql`${authUid} = ${table.id}`,
+    }),
+    pgPolicy("profiles-insert-policy", {
+      for: "insert",
+      as: "permissive",
+      to: authenticatedRole,
+      withCheck: sql`${authUid} = ${table.id}`,
+    }),
+    pgPolicy("profiles-update-policy", {
+      for: "update",
+      as: "permissive",
+      to: authenticatedRole,
+      using: sql`${authUid} = ${table.id}`,
+    }),
+    pgPolicy("profiles-delete-policy", {
+      for: "delete",
+      as: "permissive",
+      to: authenticatedRole,
+      using: sql`${authUid} = ${table.id}`,
+    }),
+  ],
+);
