babad08d1058dfe0e67d73d98ec64b26291fb7c1
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-02-03 19:17:14 +0900

    feat(wemake): add notify triggers

---
+++ apps/wemake/app/sql/triggers/notifications_trigger.sql
@@ -0,0 +1,56 @@
+CREATE FUNCTION wemake.notify_follow()
+RETURNS TRIGGER
+SECURITY DEFINER SET search_path = ''
+LANGUAGE plpgsql
+AS $$
+BEGIN
+    INSERT INTO wemake.notifications (type, source_id, target_id)
+    VALUES ('follow', NEW.follower_id, NEW.following_id);
+    RETURN NEW;
+END;
+$$;--> statement-breakpoint
+
+CREATE TRIGGER notify_follow_trigger
+AFTER INSERT ON wemake.follows
+FOR EACH ROW
+EXECUTE PROCEDURE wemake.notify_follow();--> statement-breakpoint
+
+CREATE FUNCTION wemake.notify_review()
+RETURNS TRIGGER
+SECURITY DEFINER SET search_path = ''
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    product_owner uuid;
+BEGIN
+    SELECT profile_id INTO product_owner FROM wemake.products WHERE product_id = NEW.product_id;
+    INSERT INTO wemake.notifications (type, source_id, target_id)
+    VALUES ('review', NEW.profile_id, product_owner);
+    RETURN NEW;
+END;
+$$;--> statement-breakpoint
+
+CREATE TRIGGER notify_review_trigger
+AFTER INSERT ON wemake.reviews
+FOR EACH ROW
+EXECUTE PROCEDURE wemake.notify_review();--> statement-breakpoint
+
+CREATE FUNCTION wemake.notify_reply()
+RETURNS TRIGGER
+SECURITY DEFINER SET search_path = ''
+LANGUAGE plpgsql
+AS $$
+DECLARE
+    post_owner uuid;
+BEGIN
+    SELECT profile_id INTO post_owner FROM wemake.posts WHERE post_id = NEW.post_id;
+    INSERT INTO wemake.notifications (type, source_id, target_id)
+    VALUES ('reply', NEW.profile_id, post_owner);
+    RETURN NEW;
+END;
+$$;--> statement-breakpoint
+
+CREATE TRIGGER notify_reply_trigger
+AFTER INSERT ON wemake.post_replies
+FOR EACH ROW
+EXECUTE PROCEDURE wemake.notify_reply();--> statement-breakpoint
