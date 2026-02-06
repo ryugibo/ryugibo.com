72bdba23465b4335c8bc64c351a76b58f07b89f6
ryugibo <7928632+ryugibo@users.noreply.github.com>
2026-02-07 00:52:13 +0900

    feat(den): send email from page turner

--- apps/den/app/features/page-turner/pages/page-turner-page.tsx
+++ apps/den/app/features/page-turner/pages/page-turner-page.tsx
@@ -1,6 +1,10 @@
+import { PageTurnerEmail } from "@ryugibo/emails/page-turner-email";
 import { DateTime } from "luxon";
+import { Resend } from "resend";
 import { supabaseAdmin as supabase } from "~/supabase-client.ts";
 import type { Route } from "./+types/page-turner-page";
+
+const client = new Resend(process.env.RESEND_API_KEY);
 
 export const action = async ({ request }: Route.ActionArgs) => {
   if (request.method !== "POST") {
@@ -12,42 +16,68 @@ export const action = async ({ request }: Route.ActionArgs) => {
   }
   const weekAgo = DateTime.now().minus({ weeks: 1 });
 
-  const { data, error } = await supabase
+  const { data: dataDb, error: errorDb } = await supabase
     .from("profile_books")
-    .select(`
-    profile_id,
-    book:books!book_id(title),
-    updated_at
-  `)
+    .select(
+      `
+      profile:profiles!profile_id(
+        username,
+        email
+      ),
+      book:books!book_id(
+        id,
+        title
+      ),
+      updated_at
+    `,
+    )
     .eq("read_state", "reading")
     .lte("updated_at", weekAgo.toISO())
     .order("profile_id")
     .order("updated_at", { ascending: true });
 
-  if (error) {
-    console.log(error);
+  if (errorDb) {
+    console.log(errorDb);
     return new Response(null, { status: 404 });
   }
 
-  const targets = data.reduce(
-    (acc: { profile_id: string; book_title: string }[], current) => {
-      if (!current.profile_id || !current.book?.title) {
+  const targets = dataDb.reduce(
+    (acc: { email: string; username: string; book_title: string; book_id: number }[], current) => {
+      if (!current.profile?.email || !current.book?.title) {
         return acc;
       }
-      if (acc.find((item) => item.profile_id === current.profile_id)) {
+      if (acc.find((item) => item.username === current.profile?.username)) {
         return acc;
       }
       acc.push({
-        profile_id: current.profile_id,
+        email: current.profile.email,
+        username: current.profile.username,
         book_title: current.book.title,
+        book_id: current.book.id,
       });
       return acc;
     },
-    [] as { profile_id: string; book_title: string }[],
+    [] as { email: string; username: string; book_title: string; book_id: number }[],
   );
 
-  console.log(targets);
-  // @todo: profile_id 유저에게 book_title에 대해서 알림 보내기
+  for (const target of targets) {
+    const { error } = await client.emails.send({
+      from: "Den <den@noreply.ryugibo.com>",
+      to: [target.email],
+      subject: `${target.book_title}, 뒷이야기가 궁금하지 않으신가요? 👀`,
+      react: (
+        <PageTurnerEmail
+          username={target.username}
+          book_title={target.book_title}
+          url={`${process.env.BASE_URL}/book/${target.book_id}`}
+          coverUrl={`https://github.com/ryugibo.png`} // @TODO: replace real cover url fetched from api
+        />
+      ),
+    });
+    if (error) {
+      console.log(error);
+    }
+  }
 
   return Response.json({ ok: true });
 };
